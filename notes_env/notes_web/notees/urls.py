100e8c325765c6a4da2e58a37269c780089e6be4
Mañanas <esthermurado8@gmail.com>
2024-04-16 13:49:06 +0200

    arreglos y vistas

--- notes_env/notes_web/notees/urls.py
+++ notes_env/notes_web/notees/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),  # URL para la vista de inicio
-    path('inicio/', views.pagina_inicio, name='pagina_inicio'),  # URL para la página de inicio deseada
+    path('', views.inicio, name='inicio'),
+    path('inicio/', views.pagina_inicio, name='pagina_inicio'),
     path('login/', views.inicio_sesion, name='login'),
     path('registro/', views.registro, name='registro'),
     path('lista_notas/', views.lista_notas, name='lista_notas'),
-    path('crear/', views.crear_nota, name='creanota'),  # Nueva URL para crear una nota
-    path('borrar/<int:nota_id>/', views.borrar_nota, name='borrar_nota'),  # Nueva URL para borrar una nota
+    path('crear/', views.crear_nota, name='creanota'),
+    path('editar/<int:nota_id>/', views.editar_nota, name='editar_nota'),
+    path('borrar/<int:nota_id>/', views.borrar_nota, name='borrar_nota'),
     path('nota/<int:nota_id>/', views.detalle_nota, name='detalle_nota'),
 ]
 
--- notes_env/notes_web/notees/views.py
+++ notes_env/notes_web/notees/views.py
@@ -4,6 +4,8 @@ from .forms import NotaForm
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+import time
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -16,6 +18,36 @@ def inicio(request):
 def pagina_inicio(request):
     return render(request, 'pagina_inicio.html')
 
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('inicio_sesion')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registro.html', {'form': form})
+
+
+def inicio_sesion(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('lista_notas')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.')  # Añade un mensaje de error
+    else:
+        form = AuthenticationForm()
+    return render(request, 'inicio_sesion.html', {'form': form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('inicio_sesion')
+
+@login_required
 def lista_notas(request):
     notas = Nota.objects.all()
     return render(request, 'lista_notas.html', {'notas': notas})
@@ -42,34 +74,23 @@ def borrar_nota(request, nota_id):
     nota.delete()
     return redirect('lista_notas')
 
+@login_required
+def editar_nota(request, nota_id):
+    nota = get_object_or_404(Nota, pk=nota_id)
+    if request.method == 'POST':
+        form = NotaForm(request.POST, instance=nota)
+        if form.is_valid():
+            form.save()
+            return redirect('creanota')  # Redirige al espacio de creación después de editar la nota
+    else:
+        form = NotaForm(instance=nota)
+    return render(request, 'crear_nota.html', {'form': form})
 
 @login_required
-def registro(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('lista_notas')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registro.html', {'form': form})
-
-def inicio_sesion(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('lista_notas')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'inicio_sesion.html', {'form': form})
-
-def cerrar_sesion(request):
-    logout(request)
-    return redirect('inicio_sesion')
-
 def detalle_nota(request, nota_id):
     nota = get_object_or_404(Nota, pk=nota_id)
     return render(request, 'detalle_nota.html', {'nota': nota})
+
+
+
+
